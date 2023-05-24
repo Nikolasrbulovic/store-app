bd815bf226fdbe82286904f68ce1b045a6385377
Nikolasrbulovic <111436199+Nikolasrbulovic@users.noreply.github.com>
2023-05-24 19:49:11 +0200

    delete Customer

--- store-app/src/components/AppCustomers.jsx
+++ store-app/src/components/AppCustomers.jsx
@@ -1,6 +1,29 @@
-
-
-const AppCustomers = () =>{
-    
-}
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+const AppCustomers = () => {
+  const [appCustomers, setAppCustomers] = useState([
+    "Marko",
+    "Nikola",
+    "Milos",
+    "Milan",
+  ]);
+  const deleteCustomerHandler = (name) => {
+    setAppCustomers((prev) => prev.filter((customer) => customer !== name));
+  };
+  return (
+    <ListGroup>
+      {appCustomers.map((customer, key) => {
+        return (
+          <ListGroup.Item key={key}>
+            {customer}
+            <Button onClick={() => deleteCustomerHandler(customer)}>
+              Delete Customer
+            </Button>
+          </ListGroup.Item>
+        );
+      })}
+    </ListGroup>
+  );
+};
 export default AppCustomers;
